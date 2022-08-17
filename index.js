78d32e0235593d16fd3b5b9acdb4167a56e6738e
jasonkiptoo <kiptoojason@gmail.com>
2022-08-17 08:18:54 +0300

    Completed the Assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+const num1=31;
+const num2=2;
 
-// Write your code here
+let multiply=num1*num2;
+
+
+
+// function random(){
+
+
+
+// let random=/// generate a random number 
+
+let random=Math.floor(Math.random()*10)+1;
+
+
+////modulo to get a remainder of 4
+const num3=22;
+const num4=6;
+let  mod=num3%num4;
+
+
+
+////find max number from a set of integers
+
+ let max=Math.max(1, 12, 18, 4, 20);
